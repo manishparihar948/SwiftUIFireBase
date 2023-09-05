fc259a5c989f6cdf6c1b606198155026f367ea20
manishparihar948 <manish.parihar948@gmail.com>
2023-09-05 09:52:12 +0200

    Project Setup for Firebase based app

---
+++ SwiftUIFirebaseBootcamp/SwiftUIFirebaseBootcamp/SwiftUIFirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseBootcampApp.swift
+//  SwiftUIFirebaseBootcamp
+//
+//  Created by Manish Parihar on 05.09.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
