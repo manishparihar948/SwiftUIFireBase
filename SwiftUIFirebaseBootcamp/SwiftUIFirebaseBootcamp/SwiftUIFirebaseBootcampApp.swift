d61f6d48db60ec2196bafd440690e24d3a4acf70
manishparihar948 <manish.parihar948@gmail.com>
2023-09-05 10:14:17 +0200

    Added Firebase via Swift Package manager

--- SwiftUIFirebaseBootcamp/SwiftUIFirebaseBootcamp/SwiftUIFirebaseBootcampApp.swift
+++ SwiftUIFirebaseBootcamp/SwiftUIFirebaseBootcamp/SwiftUIFirebaseBootcampApp.swift
@@ -6,12 +6,38 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct SwiftUIFirebaseBootcampApp: App {
+    
+    // register app delegate for Firebase setup
+     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    
+    // Type of way to call Fire base - Type 1
+    /*
+    init() {
+        FirebaseApp.configure()
+        print("Configured Firebase!")
+    }
+    */
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+// Type of way to call Fire base - Type 2
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+      
+    FirebaseApp.configure()
+    // print("Configured Firebase!")
+      
+    return true
+  }
 }
