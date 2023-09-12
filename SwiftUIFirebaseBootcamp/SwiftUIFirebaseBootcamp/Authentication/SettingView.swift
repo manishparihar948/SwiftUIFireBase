3176d07fb9bcadcd11c443d605e7e64525c34ff3
manishparihar948 <manish.parihar948@gmail.com>
2023-09-12 12:38:28 +0200

    Added Logout, Reset Email, Reset Password and Update Password

--- SwiftUIFirebaseBootcamp/SwiftUIFirebaseBootcamp/Authentication/SettingView.swift
+++ SwiftUIFirebaseBootcamp/SwiftUIFirebaseBootcamp/Authentication/SettingView.swift
@@ -13,6 +13,26 @@ final class SettingsViewModel: ObservableObject {
     func signOut() throws {
        try AuthenticationManager.shared.signOut()
     }
+    
+    func resetPassword() async throws {
+        let authUser = try AuthenticationManager.shared.getAuthenticatedUser()
+        
+        guard let email = authUser.email else{
+            throw URLError(.fileDoesNotExist)
+        }
+        
+        try await AuthenticationManager.shared.resetPassword(email: email)
+    }
+    
+    func updateEmail() async throws {
+        let email = "123@gmail.com"
+        try await AuthenticationManager.shared.updateEmail(email: email)
+    }
+    
+    func updatePassword() async throws {
+        let password = "123456"
+        try await AuthenticationManager.shared.updatePassword(password: password)
+    }
 }
 
 struct SettingView: View {
@@ -26,11 +46,13 @@ struct SettingView: View {
                 Task{
                     do {
                         try  viewModel.signOut()
+                        showSignInView = true
                     }catch {
                         print(error)
                     }
                 }
             }
+            emailSections
         }.navigationTitle("Settings")
     }
 }
@@ -39,6 +61,49 @@ struct SettingView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationStack {
             SettingView(showSignInView: .constant(false))
+        }
+    }
+}
+
+extension SettingView {
+    
+    private var emailSections:some View {
+        Section {
+            
+            Button("Reset password"){
+                Task{
+                    do {
+                        try  await viewModel.resetPassword()
+                        print("Password RESET")
+                    }catch {
+                        print(error)
+                    }
+                }
+            }
+            
+            Button("Update password"){
+                Task{
+                    do {
+                        try  await viewModel.updatePassword()
+                        print("Password Updated")
+                    }catch {
+                        print(error)
+                    }
+                }
+            }
+            
+            Button("Update email"){
+                Task{
+                    do {
+                        try  await viewModel.updatePassword()
+                        print("Email Updated")
+                    }catch {
+                        print(error)
+                    }
+                }
+            }
+        } header: {
+            Text("Email funtions")
         }
     }
 }
--- SwiftUIFirebaseBootcamp/SwiftUIFirebaseBootcamp/Authentication/SignInEmailView.swift
+++ SwiftUIFirebaseBootcamp/SwiftUIFirebaseBootcamp/Authentication/SignInEmailView.swift
@@ -13,26 +13,32 @@ final class SignInEmailViewModel : ObservableObject {
     @Published var email = ""
     @Published var password = ""
     
-    func signIn() {
+    func signUp() async throws {
         guard !email.isEmpty, !password.isEmpty else {
             print("No email or password found.")
             return
         }
         
-        Task {
-            do {
-                let returnedUserData = try await AuthenticationManager.shared.createUser(email: email, password: password)
-                print("Success")
-                print(returnedUserData)
-            } catch {
-                print("Error: \(error)")
-            }
+        //let _ = try await AuthenticationManager.shared.createUser(email: email, password: password)
+        //print("Success")
+        //print(returnedUserData)
+        // We can write like this if we add @discarableResult
+        try await AuthenticationManager.shared.createUser(email: email, password: password)
+       
+    }
+    
+    func signIn() async throws {
+        guard !email.isEmpty, !password.isEmpty else {
+            print("No email or password found.")
+            return
         }
+        try await AuthenticationManager.shared.signInUser(email: email, password: password)
     }
 }
 
 struct SignInEmailView: View {
     @StateObject private var viewModel = SignInEmailViewModel()
+    @Binding var showSignInView : Bool
     
     var body: some View {
         VStack {
@@ -47,7 +53,23 @@ struct SignInEmailView: View {
                 .cornerRadius(10)
             
             Button {
-                viewModel.signIn()
+                Task {
+                    do {
+                        try await  viewModel.signUp()
+                        showSignInView = false
+                        return
+                    } catch {
+                        print(error)
+                    }
+                    
+                    do {
+                        try await  viewModel.signIn()
+                        showSignInView = false
+                        return
+                    } catch {
+                        print(error)
+                    }
+                }
             } label: {
                  Text("Sign In")
                     .font(.headline)
@@ -67,7 +89,7 @@ struct SignInEmailView: View {
 struct SignInEmailView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationStack {
-            SignInEmailView()
+            SignInEmailView(showSignInView: .constant(false))
         }
     }
 }
